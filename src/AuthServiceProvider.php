8885c1e724a33dcb3ca9f7c0326a6fd709691c7d
Firdavs Ibodullaev <firdavs3253@gmail.com>
2023-01-17 10:24:52 +0500

    created service provider

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace src;
+namespace Mostbyte\Auth;
 
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
+use Mostbyte\Auth\Models\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         $this->mergeConfigFrom(__DIR__ . "/../config/mostbyte-auth.php", "mostbyte-auth");
     }
@@ -22,16 +24,36 @@ class AuthServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      *
      * @return void
+     * @throws BindingResolutionException
      */
-    public function boot()
+    public function boot(): void
     {
         if ($this->app->environment(['local'])) {
-
-            $base_url = config('services.identity.api_v1');
+            $base_url = config('mostbyte-auth.identity.base');
 
             Http::fake([
                 $base_url . "/auth/check-token" => Http::response($this->fakeResponse())
             ]);
         }
+    }
+
+    /**
+     * Get fake response for check token route
+     *
+     * @return array
+     * @throws BindingResolutionException
+     */
+    private function fakeResponse(): array
+    {
+        return [
+            "success" => true,
+            "message" => "Token is valid!",
+            "data" => [
+                "token" => app(User::class)->getToken(),
+                "refreshToken" => "s6PLorf3T1kqI84Dj9+fpwhIJc3n3pvrWqJytwXeoBy8GmH7WSDdk5ilFnXNjT5ThVm9m+UXMwJvNft9oAbECA==",
+                "tokenExpires" => "2022-11-03T11:02:01.972744Z",
+                "user" => User::attributes()
+            ]
+        ];
     }
 }
