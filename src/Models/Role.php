5e3a60c355af70b34afd8f249f5be8083380de69
Firdavs Ibodullaev <firdavs3253@gmail.com>
2023-01-17 10:26:46 +0500

    fixed

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -6,7 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'nameUz',
+        'nameRu',
+        'nameEng'
+    ];
 
+    /**
+     * @return array
+     */
     public static function attributes(): array
     {
         return [
